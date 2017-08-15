32db00014ad032f213ac462e6396016c398d554d
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-08-15 08:39:58 -0500

    Add Following model with migration.
    This will allow users to follow Artists they desire.

---
+++ Gighub/Controllers/Api/FollowingsController.cs
@@ -0,0 +1,20 @@
+﻿using Gighub.Models;
+using System.Web.Http;
+
+namespace Gighub.Controllers.Api
+{
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult CreateFollowing()
+        {
+
+        }
+    }
+}
