e1fb5a8fcc6ce8d3f58777e38bc7e3b80f1f2e0e
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-08-16 19:54:45 -0500

    Add Notification model with UserNotifications.

--- Gighub/Models/ApplicationDbContext.cs
+++ Gighub/Models/ApplicationDbContext.cs
@@ -13,6 +13,10 @@ namespace Gighub.Models
 
         public DbSet<Following> Followings { get; set; }
 
+        public DbSet<Notification> Notifications { get; set; }
+
+        public DbSet<UserNotification> UserNotifications { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -38,6 +42,11 @@ namespace Gighub.Models
             modelBuilder.Entity<ApplicationUser>()
                 .HasMany(u => u.Followees)
                 .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<UserNotification>()
+                .HasRequired(n => n.User)
+                .WithMany()
                 .WillCascadeOnDelete(false);
 
             base.OnModelCreating(modelBuilder);
