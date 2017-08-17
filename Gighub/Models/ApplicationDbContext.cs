4494bc129554fc016a264ee332f560092722c111
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-08-17 12:56:54 -0500

    Configuring reverse relationships between models.
    Each Gig has many Attendances and each User has many UserNotifications.

--- Gighub/Models/ApplicationDbContext.cs
+++ Gighub/Models/ApplicationDbContext.cs
@@ -31,7 +31,7 @@ namespace Gighub.Models
         {
             modelBuilder.Entity<Attendance>()
                 .HasRequired(a => a.Gig)
-                .WithMany()
+                .WithMany(g => g.Attendances)
                 .WillCascadeOnDelete(false);
 
             modelBuilder.Entity<ApplicationUser>()
@@ -46,7 +46,7 @@ namespace Gighub.Models
 
             modelBuilder.Entity<UserNotification>()
                 .HasRequired(n => n.User)
-                .WithMany()
+                .WithMany(u => u.UserNotifications)
                 .WillCascadeOnDelete(false);
 
             base.OnModelCreating(modelBuilder);
