eef171bb89c1783969c2104bd0f155574c778fb1
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-08-19 09:15:36 -0500

    Add Animate.css to project.
    Displaying notifications with bounceInDown animation.

--- Gighub/App_Start/BundleConfig.cs
+++ Gighub/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Gighub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/animate.css",
                       "~/Content/site.css"));
         }
     }
