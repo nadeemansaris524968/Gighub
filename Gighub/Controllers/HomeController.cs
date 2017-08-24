43853365e360875fde7f2c9b9dc80b5baaca1dee
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-08-24 11:24:22 -0500

    Refactor HomeController with AttendanceRepository.

--- Gighub/Controllers/HomeController.cs
+++ Gighub/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
+using Gighub.Repositories;
 using Microsoft.AspNet.Identity;
 
 namespace Gighub.Controllers
@@ -11,10 +12,12 @@ namespace Gighub.Controllers
     public class HomeController : Controller
     {
         private ApplicationDbContext _context;
+        private AttendanceRepository _attendanceRepository;
 
         public HomeController()
         {
             _context = new ApplicationDbContext();
+            _attendanceRepository = new AttendanceRepository(_context);
         }
         public ActionResult Index(string query = null)
         {
@@ -36,9 +39,7 @@ namespace Gighub.Controllers
 
             string userId = User.Identity.GetUserId();
 
-            var attendances = _context.Attendances
-                .Where(a => a.AttendeeId == userId && a.Gig.DateTime > DateTime.Now)
-                .ToList()
+            var attendances = _attendanceRepository.GetFutureAttendances(userId)
                 .ToLookup(a => a.GigId);
 
             var viewModel = new GigsViewModel
