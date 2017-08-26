9369d5374755e1f4790357ce1448575012e33e1d
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-08-25 21:08:31 -0500

    Using Ninject Conventions to apply DI.

--- Gighub/App_Start/NinjectWebCommon.cs
+++ Gighub/App_Start/NinjectWebCommon.cs
@@ -10,6 +10,7 @@ namespace Gighub.App_Start
 
     using Ninject;
     using Ninject.Web.Common;
+    using Ninject.Extensions.Conventions;
 
     public static class NinjectWebCommon 
     {
@@ -46,6 +47,12 @@ namespace Gighub.App_Start
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
                 RegisterServices(kernel);
+                kernel.Bind(x =>
+                {
+                    x.FromThisAssembly()
+                        .SelectAllClasses()
+                        .BindDefaultInterface();
+                });
                 return kernel;
             }
             catch
